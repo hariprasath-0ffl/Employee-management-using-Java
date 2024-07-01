65cc86ef7f2ae9aa59bab6a77ee1b059484fd36c
hariprasath <123928482+hariprasath-0ffl@users.noreply.github.com>
2024-07-01 21:57:47 +0530

    Add files via upload

---
+++ src/hr/management/system/Splash.java
@@ -0,0 +1,60 @@
+
+package hr.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Splash extends JFrame implements ActionListener {
+    
+    Splash() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("HR DATABASE MANAGEMENT SYSTEM");
+        heading.setBounds(0,200,1200,60);
+        heading.setFont(new Font("serif", Font.BOLD, 60));
+        heading.setForeground(Color.RED);
+        add(heading);
+        
+        
+        
+        JButton clickhere = new JButton("CLICK HERE TO CONTINUE");
+        clickhere.setBounds(400, 400, 300, 70);
+        clickhere.setBackground(Color.BLACK);
+        clickhere.setForeground(Color.WHITE);
+        clickhere.addActionListener(this);
+        add(clickhere);
+        
+        
+        setSize(1170, 650);
+        setLocation(200, 50);
+        setVisible(true);
+        
+        while(true) {
+            heading.setVisible(false);
+            try {
+                Thread.sleep(300);
+            } catch (Exception e){
+                 
+            }
+            
+            heading.setVisible(true);
+            try {
+                Thread.sleep(750);
+            } catch (Exception e){
+                
+            }
+        }
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
+    
+    public static void main(String args[]) {
+        new Splash();
+    }
+}
